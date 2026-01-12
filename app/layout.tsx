11b0ef7ef8b8a27db4c87380a333e73cd10eedf3
Lance Valle <vallelance8@gmail.com>
2026-01-12 22:14:14 +0800

    update then link

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { siteConfig } from "@/content/site"
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://florence-mae-and-daniel-invites.netlify.app/"
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://denmark-and-rizchelle-invitation.vercel.app/"
 const canonicalUrl = siteUrl.replace(/\/$/, "")
 const desktopHero = "/Details/LinkPreviewnew.jpg"
 const mobileHero = "/Details/LinkPreviewnew.jpg"
